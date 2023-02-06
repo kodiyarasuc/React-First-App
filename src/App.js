ed98dba1639c33c8d336e371b5d8b67c9174de06
kodiyarasu <kodiyarasu01@gmail.com>
2023-02-06 10:57:29 +0530

    Firstapp react Js is added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <h1>Learning React JS Beginner.</h1>Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
